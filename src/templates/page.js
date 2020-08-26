9b383db6261df478a637e002816602b21e8d9b1c
babono <muhammad.akbar@gmail.com>
2020-08-26 14:59:18 +0700

    update

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import notionRendererFactory from 'gatsby-source-notionso/lib/renderer';
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
 import SEO from "../components/seo"
 import NotionBlockRenderer from '../components/notionBlockRenderer';
 
